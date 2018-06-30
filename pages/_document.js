8517336a52d284421e85985e9b90fdfa5112175a
Max Freundlich <maxwfreu@alumni.stanford.edu>
2018-06-29 22:00:58 -0700

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,13 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <link rel="apple-touch-icon" sizes="76x76" href="../static/images/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../static/images/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="../static/images/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="../static/images/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="../static/images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet" />
         </Head>
